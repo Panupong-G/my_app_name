b3648e2f7d4677e9ba449ab01815f0ded3c5fd1b
Panupong <git@github.com:Panupong-G/myproject.git>
2023-02-13 16:31:51 +0700

    set branch git

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,6 +4,7 @@
 # You can define all roles on a single server, or split them:
 
   server "192.46.230.36", user: "fluke", roles: %w{app db web}, my_property: :my_value
+  set :branch, ENV['BRANCH'] || 'main'
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
